bd9f1e44b5182ccfef764e33bac582144fb01fb3
dominikg <dominik.goepel@gmx.de>
2022-08-09 00:35:25 +0200

    fix: lets see if NODE_ENV exists on windows

--- playground/ssr-vanilla/src/entry-server.js
+++ playground/ssr-vanilla/src/entry-server.js
@@ -1,10 +1,10 @@
 import { SUPER_SECRET } from 'virtual:env';
-import { PATH } from 'virtual:env:runtime';
+import { NODE_ENV } from 'virtual:env:runtime';
 export function render() {
 	const html = `
     <div>
       <h1>server env</h1>
-      <pre id="server-env">SUPER_SECRET = ${SUPER_SECRET}\nPATH = ${PATH.substring(0, 5)}...</pre>
+      <pre id="server-env">SUPER_SECRET = ${SUPER_SECRET}\nNODE_ENV = ${NODE_ENV}</pre>
       <h1>client env</h1>
       <pre id="client-env"></pre>
     </div>
